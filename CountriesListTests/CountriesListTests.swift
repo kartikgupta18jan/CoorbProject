30f7b7ccecd9d621328f2c52c4dc9f6b8b45c6f4
Kartik Gupta <rohangupta0420@gmail.com>
2025-05-27 17:45:21 +0530

    #Added unit test cases #Tested unit test cases

--- CountriesListTests/CountriesListTests.swift
+++ CountriesListTests/CountriesListTests.swift
@@ -18,16 +18,28 @@ class CountriesListTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testAddRemoveCountry() async {
-        let vm = CountryViewModel()
-        await vm.loadCountries()
+    @MainActor
+    func testAddCountry() {
+        let viewModel = CountryViewModel()
+        let country = Country(name: "France", capital: "Paris", currencies: [])
 
-        let testCountry = Country(name: "Testland", capital: "Test City", currencies: [])
+        viewModel.addCountry(country)
 
-        vm.addCountry(testCountry)
-        XCTAssertTrue(vm.selectedCountries.contains(testCountry))
+        XCTAssertEqual(viewModel.selectedCountries.count, 1)
+    }
+    
+    @MainActor
+    func testAddCountryAsync() {
+        let expectation = XCTestExpectation(description: "Add country to ViewModel")
+        let viewModel = CountryViewModel()
+        let country = Country(name: "France", capital: "Paris", currencies: [])
 
-        vm.removeCountry(testCountry)
-        XCTAssertFalse(vm.selectedCountries.contains(testCountry))
+        Task { @MainActor in
+            viewModel.addCountry(country)
+            XCTAssertEqual(viewModel.selectedCountries.count, 5)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 1.0)
     }
 }
